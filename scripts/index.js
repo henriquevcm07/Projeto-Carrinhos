7ec7deea7ba6efa36df9fe48805293e8864cb2b1
Henrique Veloso <@henriquevcm07@gmail.com>
2026-07-18 14:54:29 -0300

    Feat: seção de destaques implementada

---
+++ scripts/index.js
@@ -0,0 +1,58 @@
+//regra para filtrar apenas os disponíveis
+const apenasDisponiveis = carro => carro.status_disponibilidade === "disponivel"; 
+//criterio de ordenação por preço
+const porMaiorPreco = (a, b) => b.valor_aluguel_dia - a.valor_aluguel_dia;
+
+async function BuscarDestaques(){
+    try{
+        const request = await fetch(`http://localhost:3001/carros`);
+        const carros = await request.json();
+        const Destaques = carros.filter(apenasDisponiveis).sort(porMaiorPreco).slice(0,4);
+        Destaques.forEach((carro,indice) => {
+            renderizarDestaque(carro,indice)
+        });
+    } catch (error){
+        console.log("ocorreu um erro");
+    }
+};
+
+function renderizarDestaque(destaque,i){
+    const img = `<img src="./styles/images/${destaque.categoria}.png" >`;
+    let corCat = '43, 127, 255';
+    if(destaque.categoria ==='desenho'){
+        corCat = '254, 154, 0';
+    } else if(destaque.categoria ==='série'){
+        corCat = '173, 70, 255';
+    };
+
+    let corDisp = '0, 188, 125';
+    if(destaque.status_disponibilidade === 'manutencao'){
+        corDisp = '254, 154, 0';
+    }
+    else if(destaque.status_disponibilidade === 'alugado'){
+        corDisp = '251, 44, 54';
+    }
+    const card = document.getElementById(`D${i}`);
+    card.innerHTML = `
+        <div class="imageCard" style="background-image: url('${destaque.url_imagem}'); display: flex; justify-content: space-between;">                        
+            <div class="categoria" style='color: rgb(${corCat},1); border:1px solid rgb(${corCat},0.25); background-color: rgb(${corCat},0.1);'>${img} ${destaque.categoria}</div>
+            <div class="disponibilidade" style='color: rgb(${corDisp},1); border:1px solid rgb(${corDisp},0.25); background-color: rgb(${corDisp},0.1)'><div class='bolinha' style="background-color: rgb(${corDisp},1)"></div>${destaque.status_disponibilidade}</div>
+        </div>
+        <div class="BodyCard"">
+            <h3>${destaque.nome}</h3>
+            <p>${destaque.universo_origem}</p>
+            <div class="AlugarCard">
+                <p>por dia</p>
+                <h2>R$ ${destaque.valor_aluguel_dia}</h2>
+                <div class="btnsCard">
+                    <a href="" id="Cardbtn1"><p>Alugar</p></a>
+                    <a href="" id="Cardbtn2"><img src='./styles/images/AgendaCinza.png'></a>
+                </div>    
+            </div>
+        </div>
+    `;
+};
+
+
+BuscarDestaques();
+
